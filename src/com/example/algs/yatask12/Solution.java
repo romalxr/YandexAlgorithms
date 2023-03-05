754070c99ae7852d221f7408c22edb8898de13e8
romalxr <romalxr@gmail.com>
2023-03-05 08:51:35 +0300

    init

---
+++ src/com/example/algs/yatask12/Solution.java
@@ -0,0 +1,31 @@
+package com.example.algs.yatask12;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+        String line = scanner.nextLine();
+
+        for (char c : line.toCharArray()) {
+            Character checked = null;
+            switch (c) {
+                case '(', '[', '{' -> stack.push(c);
+                case ')' -> checked = '(';
+                case ']' -> checked = '[';
+                case '}' -> checked = '{';
+                default -> throw new IllegalStateException("Unexpected value: " + c);
+            }
+            if (checked != null) {
+                if (stack.empty() || !checked.equals(stack.pop())) {
+                    stack.push(' ');
+                    break;
+                }
+            }
+        }
+        System.out.println(stack.empty() ? "yes" : "no");
+    }
+}
---
+++ src/com/example/algs/yatask13/Solution.java
@@ -0,0 +1,22 @@
+package com.example.algs.yatask13;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] tokens = line.split(" ");
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            switch (token) {
+                case "-" -> stack.push(-1 * stack.pop() + stack.pop());
+                case "+" -> stack.push(stack.pop() + stack.pop());
+                case "*" -> stack.push(stack.pop() * stack.pop());
+                default -> stack.push(Integer.parseInt(token));
+            }
+        }
+        System.out.println(stack.pop());
+    }
+}
